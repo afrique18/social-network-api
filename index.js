f07f75c1988d828abb0aece40008d1c8c8ddbca4
afrique18 <vincentv1225@gmail.com>
2023-08-03 14:02:11 -0400

    Add and modified data for user routes

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('social-network-api')
   ? cwd.split('social-network-api')[1]
   : cwd;
