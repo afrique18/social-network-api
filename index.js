37c1eb669be8ae8cec7aeed5b9e1033d54eda2eb
afrique18 <vincentv1225@gmail.com>
2023-08-05 12:13:47 -0400

    Created package json

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const activity = cwd.includes('social-network-api')
-  ? cwd.split('social-network-api')[1]
-  : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server for running on port ${PORT}!`);
   });
 });
